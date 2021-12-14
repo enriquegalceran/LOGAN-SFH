fa4a8b8ebec7a06a3ca211db93819e17406aeb0c
enriquegalceran <44613764+enriquegalceran@users.noreply.github.com>
2021-12-14 13:01:24 +0100

    dense-act-batchnorm-dropout combo + magnitudes branch

--- XAVIER.py
+++ XAVIER.py
@@ -24,16 +24,24 @@ import os
 class Cerebro:
     @staticmethod
     def conv_activation_pool(layer_input, nf=32, sf=3,
-                             st=1, act="relu", ps=3, do=0.25, pd="same"):
-        x = Conv1D(filters=nf, kernel_size=sf, strides=st, padding=pd)(layer_input)
+                             st=1, act="relu", ps=3, do=0.25, pdd="same"):
+        x = Conv1D(filters=nf, kernel_size=sf, strides=st, padding=pdd)(layer_input)
         x = Activation(act)(x)
-        x = BatchNormalization(axis=1)(x)
+        x = BatchNormalization()(x)
         x = MaxPooling1D(pool_size=ps)(x)
         x = Dropout(do)(x)
         return x
 
     @staticmethod
-    def build_input_spectrum_branch(inputs):
+    def dense_act_batchnorm_dropout(inputs, neurons, activation="relu", dropout=0.5):
+        x = Dense(neurons)(inputs)
+        x = Activation(activation)(x)
+        x = BatchNormalization()(x)
+        x = Dropout(dropout)(x)
+        return x
+
+    @staticmethod
+    def build_input_spectrum_branch(inputs, number_neurons, final_act="relu"):
 
         # CONV => RELU => POOL (I)
         x = Cerebro.conv_activation_pool(inputs, 32, 3, 1, "relu", 3, 0.25, "same")
@@ -44,11 +52,24 @@ class Cerebro:
         # CONV => RELU => POOL (III)
         x = Cerebro.conv_activation_pool(x, 32, 3, 1, "relu", 3, 0.25, "same")
 
+        # Output from input spectrum branch
+        x = Dense(number_neurons)(x)
+        x = Activation(final_act)(x)
         return x
 
     @staticmethod
-    def build_input_magnitudes_branch(inputs,):
-        pass
+    def build_input_magnitudes_branch(inputs, number_neurons, final_act="relu"):
+
+        # 256 neurons, relu, 50% dropout
+        x = Cerebro.dense_act_batchnorm_dropout(inputs, 256, "relu", 0.5)
+
+        # 126 neurons, relu, 25% dropout
+        x = Cerebro.dense_act_batchnorm_dropout(x, 126, "relu", 0.25)
+
+        # Output from input magnitudes branch
+        x = Dense(number_neurons)(x)
+        x = Activation(final_act)(x)
+        return x
 
     @staticmethod
     def build_output_sfh_branch(inputs,):
