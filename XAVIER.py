14f6d38b4b8e42a277eab15899d3187c5f0a863e
enriquegalceran <44613764+enriquegalceran@users.noreply.github.com>
2021-12-15 13:23:13 +0100

    Small modification that allows to define explicitly the Activation used (for the images generated). If explicit:     Activations are added as an individual layer else:     The activation is baked into the previous layer
    By default, Activation=False

--- XAVIER.py
+++ XAVIER.py
@@ -23,88 +23,117 @@ class Cerebro:
     @staticmethod
     def conv_activation_pool(layer_input: int, nf: int = 32, sf: int = 3,
                              st: int = 1, act: str = "relu", ps: int = 3,
-                             do: float = 0.25, pdd: str = "same"):
-        x = Conv1D(filters=nf, kernel_size=sf, activation=act, strides=st, padding=pdd)(layer_input)
-        x = BatchNormalization()(x)
-        x = MaxPooling1D(pool_size=ps)(x)
-        x = Dropout(do)(x)
+                             do: float = 0.25, pdd: str = "same", explicit: bool = False):
+        if explicit:
+            x = Conv1D(filters=nf, kernel_size=sf, strides=st, padding=pdd)(layer_input)
+            x = Activation(activation=act)(x)
+            x = BatchNormalization()(x)
+            x = MaxPooling1D(pool_size=ps)(x)
+            x = Dropout(do)(x)
+        else:
+            x = Conv1D(filters=nf, kernel_size=sf, activation=act, strides=st, padding=pdd)(layer_input)
+            x = BatchNormalization()(x)
+            x = MaxPooling1D(pool_size=ps)(x)
+            x = Dropout(do)(x)
         return x
 
     @staticmethod
-    def dense_act_batchnorm_dropout(inputs: int, neurons: int, activation: str = "relu", dropout: float = 0.5):
-        x = Dense(neurons, activation=activation)(inputs)
-        x = BatchNormalization()(x)
-        x = Dropout(dropout)(x)
+    def dense_act_batchnorm_dropout(inputs: int, neurons: int, act: str = "relu",
+                                    dropout: float = 0.5, explicit: bool = False):
+        if explicit:
+            x = Dense(neurons)(inputs)
+            x = Activation(activation=act)(x)
+            x = BatchNormalization()(x)
+            x = Dropout(dropout)(x)
+        else:
+            x = Dense(neurons, activation=act)(inputs)
+            x = BatchNormalization()(x)
+            x = Dropout(dropout)(x)
         return x
 
     @staticmethod
-    def build_input_spec_branch(inputs, number_neurons: int, final_act: str = "relu"):
+    def build_input_spec_branch(inputs, number_neurons: int, final_act: str = "relu", explicit: bool = False):
         # CONV => RELU => POOL (I)
-        x = Cerebro.conv_activation_pool(inputs, 64, 10, 1, "relu", 3, 0.25, "same")
+        x = Cerebro.conv_activation_pool(inputs, 64, 10, 1, "relu", 3, 0.25, "same", explicit)
 
         # CONV => RELU => POOL (II)
-        x = Cerebro.conv_activation_pool(x, 32, 5, 1, "relu", 3, 0.25, "same")
+        x = Cerebro.conv_activation_pool(x, 32, 5, 1, "relu", 3, 0.25, "same", explicit)
 
         # CONV => RELU => POOL (III)
-        x = Cerebro.conv_activation_pool(x, 32, 3, 1, "relu", 3, 0.10, "same")
+        x = Cerebro.conv_activation_pool(x, 32, 3, 1, "relu", 3, 0.10, "same", explicit)
 
         # Output from input spectrum branch
         x = Flatten()(x)
-        x = Dense(number_neurons, activation=final_act, name="spectra_intermediate")(x)
+        if explicit:
+            x = Dense(number_neurons)(x)
+            x = Activation(activation=final_act, name="spectra_intermediate")(x)
+        else:
+            x = Dense(number_neurons, activation=final_act, name="spectra_intermediate")(x)
         return x
 
     @staticmethod
-    def build_input_magn_branch(inputs: int, number_neurons: int, final_act: str = "relu"):
+    def build_input_magn_branch(inputs: int, number_neurons: int, final_act: str = "relu", explicit: bool = False):
         # 256 neurons, relu, 50% dropout
-        x = Cerebro.dense_act_batchnorm_dropout(inputs, 128, "relu", 0.5)
+        x = Cerebro.dense_act_batchnorm_dropout(inputs, 128, "relu", 0.5, explicit)
 
         # 126 neurons, relu, 25% dropout
-        x = Cerebro.dense_act_batchnorm_dropout(x, 64, "relu", 0.25)
+        x = Cerebro.dense_act_batchnorm_dropout(x, 64, "relu", 0.25, explicit)
 
         # 32 neurons, relu, 10% dropout
-        x = Cerebro.dense_act_batchnorm_dropout(x, 32, "relu", 0.25)
+        x = Cerebro.dense_act_batchnorm_dropout(x, 32, "relu", 0.25, explicit)
 
         # Output from input magnitudes branch
         x = Flatten()(x)
-        x = Dense(number_neurons, activation=final_act, name="magnitude_intermediate")(x)
+        if explicit:
+            x = Dense(number_neurons)(x)
+            x = Activation(activation=final_act, name="magnitude_intermediate")(x)
+        else:
+            x = Dense(number_neurons, activation=final_act, name="magnitude_intermediate")(x)
         return x
 
     @staticmethod
-    def build_output_sfh_branch(inputs: int, number_neurons: int, final_act: str = "linear"):
+    def build_output_sfh_branch(inputs: int, number_neurons: int, final_act: str = "linear", explicit: bool = False):
         # 512 neurons, relu, 50% dropout
-        x = Cerebro.dense_act_batchnorm_dropout(inputs, 512, "relu", 0.5)
+        x = Cerebro.dense_act_batchnorm_dropout(inputs, 512, "relu", 0.5, explicit)
 
         # 256 neurons, relu, 25% dropout
-        x = Cerebro.dense_act_batchnorm_dropout(x, 256, "relu", 0.25)
+        x = Cerebro.dense_act_batchnorm_dropout(x, 256, "relu", 0.25, explicit)
 
         # 256 neurons, relu, 25% dropout
-        x = Cerebro.dense_act_batchnorm_dropout(x, 256, "relu", 0.25)
+        x = Cerebro.dense_act_batchnorm_dropout(x, 256, "relu", 0.25, explicit)
 
         # 128 neurons, relu, 10% dropout
-        x = Cerebro.dense_act_batchnorm_dropout(x, 128, "relu", 0.10)
+        x = Cerebro.dense_act_batchnorm_dropout(x, 128, "relu", 0.10, explicit)
 
         # Output from output sfh branch
-        x = Dense(number_neurons)(x)
-        x = Activation(final_act, name="sfh_output")(x)
+        if explicit:
+            x = Dense(number_neurons)(x)
+            x = Activation(activation=final_act, name="sfh_output")(x)
+        else:
+            x = Dense(number_neurons, activation=final_act, name="sfh_output")(x)
         return x
 
     @staticmethod
-    def build_output_metallicity_branch(inputs: int, number_neurons: int, final_act: str = "linear"):
+    def build_output_metallicity_branch(inputs: int, number_neurons: int,
+                                        final_act: str = "linear", explicit: bool = False):
         # 512 neurons, relu, 50% dropout
-        x = Cerebro.dense_act_batchnorm_dropout(inputs, 512, "relu", 0.5)
+        x = Cerebro.dense_act_batchnorm_dropout(inputs, 512, "relu", 0.5, explicit)
 
         # 256 neurons, relu, 25% dropout
-        x = Cerebro.dense_act_batchnorm_dropout(x, 256, "relu", 0.25)
+        x = Cerebro.dense_act_batchnorm_dropout(x, 256, "relu", 0.25, explicit)
 
         # 256 neurons, relu, 25% dropout
-        x = Cerebro.dense_act_batchnorm_dropout(x, 256, "relu", 0.25)
+        x = Cerebro.dense_act_batchnorm_dropout(x, 256, "relu", 0.25, explicit)
 
         # 128 neurons, relu, 10% dropout
-        x = Cerebro.dense_act_batchnorm_dropout(x, 128, "relu", 0.10)
+        x = Cerebro.dense_act_batchnorm_dropout(x, 128, "relu", 0.10, explicit)
 
         # Output from output sfh branch
-        x = Dense(number_neurons)(x)
-        x = Activation(final_act, name="metallicity_output")(x)
+        if explicit:
+            x = Dense(number_neurons)(x)
+            x = Activation(activation=final_act, name="metallicity_output")(x)
+        else:
+            x = Dense(number_neurons, activation=final_act, name="metallicity_output")(x)
         return x
 
     @staticmethod
