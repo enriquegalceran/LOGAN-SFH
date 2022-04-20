ea973a9eb63955c99d1671bb17380fd519c74ae0
enriquegalceran <44613764+enriquegalceran@users.noreply.github.com>
2022-04-20 11:26:17 +0200

    Create DataGeneration.R

---
+++ DataGeneration.R
@@ -0,0 +1,17 @@
+# If this ends up being a long code, maybe rename it to [James "Logan"] Howlett
+
+# Data generation for the training of Cerebro.
+# Burst relation based on the data infered from https://arxiv.org/pdf/2104.09536.pdf from figure 2 (p. 8)
+# Most frequent cases for current SFR ranges from 1e-4 to 1e-2 Msol/yr/kpc^2 for 1e7 to 1e9 total mass of Msol/kpc^2
+
+setwd("~/Documents/GitHub/LOGAN-SFH")
+source("MUTANTS/LOGAN.R")
+library(ProSpect)
+library(ggplot2)
+EMILESCombined = readRDS(file="EMILESData/EMILESCombined.rds")
+
+
+outputFolder = "/Volumes/Elements/TrainingData/"
+
+
+
