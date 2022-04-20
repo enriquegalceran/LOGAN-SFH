b9d52d9b738ae98f55ac64884278b04ae3105bc1
enriquegalceran <44613764+enriquegalceran@users.noreply.github.com>
2022-04-20 11:51:22 +0200

    Update DataGeneration.R

--- DataGeneration.R
+++ DataGeneration.R
@@ -6,12 +6,20 @@
 
 setwd("~/Documents/GitHub/LOGAN-SFH")
 source("MUTANTS/LOGAN.R")
+require(plyr)
 library(ProSpect)
 library(ggplot2)
+library(plyr)         # splat(func)(c(var1, list_of_vars))
 EMILESCombined = readRDS(file="EMILESData/EMILESCombined.rds")
 
 
 outputFolder = "/Volumes/Elements/TrainingData/"
+configFilename = "Data_Generation_Parameters.R"
+
+# Load Parameters from the Config File.
+source(configFilename)
+
+
 
 
 
