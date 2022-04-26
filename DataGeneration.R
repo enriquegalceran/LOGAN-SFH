cf83bd61f047c53f65913c110468959cddc915e4
enriquegalceran <44613764+enriquegalceran@users.noreply.github.com>
2022-04-26 13:01:24 +0200

    Added Draw function for the resulting SFHs

--- DataGeneration.R
+++ DataGeneration.R
@@ -49,13 +49,11 @@ generateDataFrameArguments <- function(Parameters,
     evaluated = splat(x$func)(c(1, x[-which(names(x)=="func")]))
     
     # Verify it is located within min/max
-    if (is.null(additional_arguments$min_val)){
     if (!is.null(additional_arguments$min_val)){
       if (evaluated < additional_arguments$min_val){
         evaluated <- additional_arguments$min_val
       }
     }
-    if (is.null(additional_arguments$max_val)){
     if (!is.null(additional_arguments$max_val)){
       if (evaluated > additional_arguments$max_val){
         evaluated <- additional_arguments$max_val
@@ -63,7 +61,6 @@ generateDataFrameArguments <- function(Parameters,
     }
     
     # Evaluate exponent
-    if (islog10) {
     if (additional_arguments$islog10) {
       evaluated <- 10**evaluated
     }
@@ -201,7 +198,7 @@ generateDataFrameArguments <- function(Parameters,
   
   
   ##### Initialize data.frame with values that are going to differ with each case ####
-  argument_names = c()
+  argument_names = c("mSFR")
   for (n in names(Parameters)){
     if (length(Parameters[[n]])> 1){
       argument_names <- c(argument_names, n)
@@ -238,16 +235,109 @@ generateDataFrameArguments <- function(Parameters,
 }
 
 
+drawSFHFromDataFrame <- function(df,
+                                 func,
+                                 speclib=NULL,
+                                 agevec=NULL,
+                                 specificCases=NULL,
+                                 progress_verbose=1000,
+                                 alpha_color=10,
+                                 ...){
+  # Draws the SFHs that are going to be used
+  dots = list(...)
+  
+  if (is.null(speclib) && is.null(agevec)){
+    stop("Either 'speclib' or 'agevec' needs to be given to the function")
+  } else {
+    if (is.null(agevec)){
+      agevec <- speclib$Age
+    }
+  }
+  
+  if (!is.null(specificCases)){
+    df <- df[specificCases, ]
+  }
+  
+  # Evaluate the values of each of the SFH
+  points_matrix <- matrix(0, nrow=dim(df)[1], ncol=length(agevec))
+  args_func <- names(df) %in% names(formals(func))
+  i <-  1
+  cat("Painting...\n")
+  while (i <= dim(df)[1]){
+    points_matrix[i, ] <- do.call("func", c(list(age=agevec), df[i, args_func]))
+    if (!is.null(progress_verbose)){
+      if (i %% progress_verbose == 0){
+        cat(paste0("Calculating: ", i, "\n"))
+      }
+    }
+    i <- i + 1
+  }
+  
+  
+  # Populate dots with default variables in case they are not defined
+  # This avoids multiple instances of the same argument, and the possibility to change them
+  plot_def_arguments = list(type="l",
+                            xlab="Age", ylab="SFR",
+                            col=mycol <- rgb(0, 0, 255, max=255, alpha=alpha_color),
+                            xlim=c(agevec[1],14e9),
+                            ylim=c(0, max(points_matrix)),
+                            log="x")
+  for (def_arg in names(plot_def_arguments)){
+    if (def_arg %!in% names(dots)){
+      dots[def_arg] <- plot_def_arguments[def_arg]
+    }
+  }
+  
+  
+  # Initialize plot
+  do.call("plot", c(list(x=agevec, y=points_matrix[1,]), dots))
+  
+  # Iterate over each line and draw the data
+  i <- 2
+  col.lines <- dots[["col"]]
+  while (i <= dim(points_matrix)[1]){
+    do.call("lines", c(list(x=agevec, y=points_matrix[i, ], col=col.lines)))
+    if (!is.null(progress_verbose)){
+      if (i %% progress_verbose == 0){
+        cat(paste0("Drawing: ", i, "\n"))
+      }
+    }
+    i <- i + 1
+  }
+  return(points_matrix)
+}
 
 
+# drawSFHFromDataFrame(df, massfunc_snorm_burst, agevec=EMILESCombined$Age, main="Test1234")
+
+
+
+generateSpecFromDataFrame <- function(Parameters,
+                                      df){
+  # Generate the Spectra from the matrix with the specific arguments
+  
+  
+  
+  
+}
+
+
+
+
+#### Execute Code ####
 df <- generateDataFrameArguments(Parameters=Parameters,
-                                 n.simul=10000,
+                                 n.simul=1000,
                                  speclib=EMILESCombined,
-                                 save_path=file.path(outputFolder, savefilename),
+                                 # save_path=file.path(outputFolder, savefilename),
                                  verbose=1,
                                  progress_verbose = 1000)
 
+point_matrix <- drawSFHFromDataFrame(df,
+                                     Parameters$massfunc,
+                                     agevec=EMILESCombined$Age,
+                                     log="xy",
+                                     ylim=c(1e-4, 15))
 
-
+# abline(v=EMILESCombined$Age)
 
 
