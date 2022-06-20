2b154d1966bd2205e3a33bd6b903b8a0d563c39f
enriquegalceran <44613764+enriquegalceran@users.noreply.github.com>
2022-06-20 16:11:10 +0200

    Parallelize Script multiple times

--- Parallelize_Script.R
+++ Parallelize_Script.R
@@ -4,9 +4,13 @@ plan(multisession)
 setwd("~/Documents/GitHub/LOGAN-SFH")
 
 n <- 4L
+iteration.times <- 3L
 
-empty <- future_replicate(n, {
-    print(.Random.seed)
-    source(file = "DataGeneration.R")
-})
-
+cat(paste0("Initiating Parallel processing with ", n, " threads...\n"))
+for (i in 1:iteration.times){
+  cat(paste0("Iteration #", i, " with ", n, " threads...\n"))
+  empty <- future_replicate(n, {
+      print(.Random.seed)
+      source(file = "DataGeneration.R")
+  })
+}
